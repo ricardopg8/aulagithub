6d8ec86be34be588be5c5c8bde19e75ee7f46c52
Ricardo Perin <ricardopg8@gmail.com>
2025-04-11 15:23:38 -0300

    Go now

--- SellingSite/obj/Debug/net8.0/SellingSite.AssemblyInfo.cs
+++ SellingSite/obj/Debug/net8.0/SellingSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SellingSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aa3af112299c30377f997cc1d2d7a34ab0bc606")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c2a4864ed535f0d2c99ff7ad4b10cb620bdb708")]
 [assembly: System.Reflection.AssemblyProductAttribute("SellingSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SellingSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
